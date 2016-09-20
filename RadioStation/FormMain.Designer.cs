c6cf736c5258381977036e722ca111047c719f3a
Anwar Buchoo <luckyhuss@msn.com>
2016-09-20 16:22:45 +0400

    All project files

---
+++ RadioStation/FormMain.Designer.cs
@@ -0,0 +1,104 @@
+﻿namespace RadioStation
+{
+    partial class FormMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormMain));
+            this.listBoxStations = new System.Windows.Forms.ListBox();
+            this.buttonExit = new System.Windows.Forms.Button();
+            this.windowsMediaPlayer = new AxWMPLib.AxWindowsMediaPlayer();
+            this.buttonRecord = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.windowsMediaPlayer)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // listBoxStations
+            // 
+            this.listBoxStations.FormattingEnabled = true;
+            this.listBoxStations.Location = new System.Drawing.Point(12, 12);
+            this.listBoxStations.Name = "listBoxStations";
+            this.listBoxStations.Size = new System.Drawing.Size(136, 147);
+            this.listBoxStations.TabIndex = 0;
+            this.listBoxStations.DoubleClick += new System.EventHandler(this.listBoxStations_DoubleClick);
+            // 
+            // buttonExit
+            // 
+            this.buttonExit.Location = new System.Drawing.Point(163, 136);
+            this.buttonExit.Name = "buttonExit";
+            this.buttonExit.Size = new System.Drawing.Size(75, 23);
+            this.buttonExit.TabIndex = 1;
+            this.buttonExit.Text = "Exit";
+            this.buttonExit.UseVisualStyleBackColor = true;
+            this.buttonExit.Click += new System.EventHandler(this.buttonExit_Click);
+            // 
+            // windowsMediaPlayer
+            // 
+            this.windowsMediaPlayer.Enabled = true;
+            this.windowsMediaPlayer.Location = new System.Drawing.Point(12, 174);
+            this.windowsMediaPlayer.Name = "windowsMediaPlayer";
+            this.windowsMediaPlayer.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("windowsMediaPlayer.OcxState")));
+            this.windowsMediaPlayer.Size = new System.Drawing.Size(226, 63);
+            this.windowsMediaPlayer.TabIndex = 2;
+            // 
+            // buttonRecord
+            // 
+            this.buttonRecord.Location = new System.Drawing.Point(163, 107);
+            this.buttonRecord.Name = "buttonRecord";
+            this.buttonRecord.Size = new System.Drawing.Size(75, 23);
+            this.buttonRecord.TabIndex = 3;
+            this.buttonRecord.Text = "Record";
+            this.buttonRecord.UseVisualStyleBackColor = true;
+            this.buttonRecord.Click += new System.EventHandler(this.buttonRecord_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(253, 245);
+            this.Controls.Add(this.buttonRecord);
+            this.Controls.Add(this.windowsMediaPlayer);
+            this.Controls.Add(this.buttonExit);
+            this.Controls.Add(this.listBoxStations);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.windowsMediaPlayer)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBoxStations;
+        private System.Windows.Forms.Button buttonExit;
+        private AxWMPLib.AxWindowsMediaPlayer windowsMediaPlayer;
+        private System.Windows.Forms.Button buttonRecord;
+    }
+}
+
---
+++ RadioStation/FormMain.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+using System.Diagnostics;
+
+namespace RadioStation
+{
+    public partial class FormMain : Form
+    {
+        private string title { get; set; }
+        public FormMain()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // set title
+            title = "Radio station";
+            this.Text = title;
+
+            using (StreamReader sReader = new StreamReader("radiolist.ini"))
+            {
+                //create a string object that would hold the
+                //value of each line in our file
+                string line = String.Empty;
+                string[] text = null;
+
+                // set the data member to display and hide
+                listBoxStations.DisplayMember = "Key";
+                listBoxStations.ValueMember = "Value";
+
+                //iterate for each line item in our file
+                while ((line = sReader.ReadLine()) != null)
+                {
+                    text = line.Split(';'); // get radio name and url
+                    if (text != null && text.Length == 2)
+                    {
+                        // read data
+                        listBoxStations.Items.Add(new KeyValuePair<string, string>(text[0], text[1]));
+                    }
+                }
+            }
+        }
+
+        private void listBoxStations_DoubleClick(object sender, EventArgs e)
+        {
+            if (listBoxStations.SelectedItem != null)
+            {
+                // get value pair for the selected item
+                KeyValuePair<string, string> kvp = (KeyValuePair<string, string>)listBoxStations.SelectedItem;
+                
+                // get the url to play
+                string url = kvp.Value;
+                this.Text = string.Format("{0} - {1}", title, kvp.Key);
+
+                // play url
+                windowsMediaPlayer.URL = url;
+            }
+        }
+
+        private void buttonExit_Click(object sender, EventArgs e)
+        {
+            // close everything before exit
+            windowsMediaPlayer.close();            
+            this.Close();
+        }
+
+        private void buttonRecord_Click(object sender, EventArgs e)
+        {
+            // open goldwave
+            Process.Start(@"C:\Program Files\GoldWave\GoldWave.exe");
+        }
+    }
+}
